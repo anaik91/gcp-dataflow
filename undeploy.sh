d961f58cb02dc9c02855c2f94b6953dbbaa82228
Ashwin Naik <ashwinknaik@google.com>
2021-06-10 18:41:14 +0530

    added undeploy

--- undeploy.sh
+++ undeploy.sh
@@ -5,15 +5,14 @@ PROTOBUF_FILE="schema.proto"
 PROTOBUF_BQ_PLUGIN="/Users/ashwinknaik/go/bin/protoc-gen-bq-schema"
 TF_DIR="tf"
 
-deploy_tf() {
+undeploy_tf() {
     terraform init
     terraform plan
-    terraform apply -auto-approve
-    terraform output -json > ${BASEDIR}/output.json
+    terraform destroy -auto-approve
 }
 
 echo -e "generating Big Query Schema for ${BASEDIR}/${PROTOBUF_DIR}/${PROTOBUF_FILE}"
 protoc --plugin=${PROTOBUF_BQ_PLUGIN} --bq-schema_out=${BASEDIR}/${TF_DIR} --proto_path=${BASEDIR}/${PROTOBUF_DIR} ${PROTOBUF_FILE}
 
 cd ${BASEDIR}/${TF_DIR}
-deploy_tf
+undeploy_tf
